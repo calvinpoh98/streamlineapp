4ef7fd9840a7de3c01853434e1f027ddb42e3cca
calvinpoh98 <calvinpoh01@gmail.com>
2022-09-17 15:45:58 +0800

    update 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,24 @@
-import streamlit as st
-import requests
-import json
 
-# Title of the page
-st.title("🎓 Graduate School Admissions")
+# Code to post the user inputs to the API and get the predictions
+# Paste the URL to your GCP Cloud Run API here!
+api_url = 'https://grad-school-admission-setncat3kq-as.a.run.app'
+api_route = '/predict'
+
+response = requests.post(f'{api_url}{api_route}', json=json.dumps(user_input)) # json.dumps() converts dict to JSON
+predictions = response.json()
+
+# Add a submit button
+if st.button("Submit"): 
+    st.write(f"Prediction: {predictions['predictions'][0]}")
+
+# Code to post the user inputs to the API and get the predictions
+# Paste the URL to your GCP Cloud Run API here!
+api_url = 'https://grad-school-admission-setncat3kq-as.a.run.app'
+api_route = '/predict'
+
+response = requests.post(f'{api_url}{api_route}', json=json.dumps(user_input)) # json.dumps() converts dict to JSON
+predictions = response.json()
+
+# Add a submit button
+if st.button("Submit"): 
+    st.write(f"Prediction: {predictions['predictions'][0]}")
